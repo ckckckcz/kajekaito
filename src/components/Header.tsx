0cf388b4c9cdc43b4c46bde661131a0a6d82c20e
ckckckcz <rriovld@gmail.com>
2024-08-02 12:20:31 +0700

    update: change background and change font style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ export default function Header() {
   return (
     <section className="header">
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 className="mb-2 text-4xl font-semibold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-3xl dark:text-white">Hey, Riovaldo here!</h1>
-        <h1 className="text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-8xl dark:text-white">
+        <h1 className="mb-2 text-4xl font-semibold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-3xl dark:text-white text-semibold">Hey, Riovaldo here!</h1>
+        <h1 className="text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-8xl dark:text-white text-bold">
           UI/UX <br /> Designer
         </h1>
-        <h1 className="mt-6 text-4xl font-semibold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-4xl dark:text-white">based in Malang</h1>
+        <h1 className="mt-6 text-4xl font-semibold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-4xl dark:text-white text-semibold">based in Malang</h1>
       </div>
     </section>
   );
