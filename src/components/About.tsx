9e1b4abfaa2ccc94854b1987c0b877576a355706
ckckckcz <rriovld@gmail.com>
2024-08-02 14:07:12 +0700

    update: deskripsi perjalanan hidup

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,22 +10,15 @@ const About: React.FC = () => {
             <img src={Profile} alt="Profile Image" className="profile-img" />
           </div>
           <div className="flex flex-col justify-center">
-            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
-            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-            <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0">
-              <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                Get started
-                <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                </svg>
-              </a>
-              <a
-                href="#"
-                className="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-              >
-                Learn more
-              </a>
-            </div>
+            <h1 className="mb-4 text-4xl text-bold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-5xl dark:text-white">Perjalanan Saya</h1>
+            <p className="mb-8 text-lg text-semibold text-gray-400 lg:text-xl dark:text-gray-400">
+              <b className="text-gray-900 text-semibold">Didorong oleh rasa ingin tahu</b> dan pensaran terhadap IT, rasa penasaran saya dimulai dengan membuat website menggunakan HTML, CSS, dan JavaScript.
+              <br />
+              <br />
+              Saat ini, saya sedang mendalami dunia IT sebagai mahasiswa <b className="text-gray-900 text-semibold">Teknik Informatika</b> di <b className="text-gray-900 text-semibold">Politeknik Negeri Malang</b>, dan sekarang Saya fokus
+              pada bidang <b className="text-gray-900 text-semibold">UI/UX Design</b>, <b className="text-gray-900 text-semibold">Front-End Web Developer</b>, dan
+              <b className="text-gray-900 text-semibold"> Kecerdasan Buatan (AI)</b>.
+            </p>
           </div>
         </div>
       </div>
