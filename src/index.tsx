8455b73736ac8337db05982fd2c2831347dc8144
ckckckcz <rriovld@gmail.com>
2024-08-02 13:45:17 +0700

    feat: about section

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import "./css/index.css";
 import "flowbite";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import About from "./components/About";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Navbar />
     <Header />
+    <About />
   </React.StrictMode>
 );
