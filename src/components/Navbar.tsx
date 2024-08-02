905d67d51722567621d06add73c8d5d844c0e066
ckckckcz <rriovld@gmail.com>
2024-08-02 11:40:23 +0700

    update: text navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,24 +31,24 @@ export default function Navbar() {
           </button>
         </div>
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 gap-4">
             <li>
-              <a href="#" className="block py-2 px-3 md:p-0 text-gray-90 rounded">
+              <a href="#" className="block py-2 px-3 md:p-0 text-gray-90 font-bold">
                 Beranda
               </a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 md:p-0 text-gray-90 rounded">
+              <a href="#" className="block py-2 px-3 md:p-0 text-gray-90 font-bold">
                 Jejak
               </a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 md:p-0 text-gray-90 rounded">
+              <a href="#" className="block py-2 px-3 md:p-0 text-gray-90 font-bold">
                 Portfolio
               </a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 md:p-0 text-gray-90 rounded">
+              <a href="#" className="block py-2 px-3 md:p-0 text-gray-90 font-bold">
                 Kontak
               </a>
             </li>
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import "flowbite";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Navbar />
+    <Header />
   </React.StrictMode>
 );
